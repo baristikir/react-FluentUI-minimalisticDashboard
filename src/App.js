cb8eb33888aaad832f3241d9d010764680c0e85c
Baris Tikir <brstkr@icloud.com>
2020-08-11 20:32:57 +0200

    Added Dashboard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'office-ui-fabric-react/dist/css/fabric.css';
 import './App.css';
+
+import Navigation from './components/navigation';
+import CardsSection from './components/CardSection';
+import OperationTable from './components/OperationTable';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ms-Grid" dir="ltr">
+      <div className="md-Grid-row">
+        <div className="ms-Grid-col ms-sm1 ms-xl1">
+          <Navigation />
+        </div>
+        <div className="main-element ms-Grid-col ms-sm11 ms-xl11">
+          <div className="ms-Grid-row">
+            <CardsSection />
+          </div>
+          <div className="ms-Grid-row">
+            <OperationTable />
+          </div>
+        </div>
+      </div> 
     </div>
   );
 }
